0dc9b73b40f3300042ade8d09b2bb2724800c29f
achjiang <htxz_jiang@163.com>
2018-10-19 18:59:07 +0800

    news and auth: add admin parameter

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -1,7 +1,39 @@
 from django.contrib import admin
 from . import models
-# Register your models here.
-admin.site.register(models.NewCategory)
-admin.site.register(models.News)
-admin.site.register(models.Banner)
-admin.site.register(models.Comment)
+
+
+class NewCategoryAdmin(admin.ModelAdmin):
+	"""NewCategory的列展示"""
+	list_display = ('id', 'name',)
+	list_filter = ('name',)
+	search_fields = ('username',)
+	list_per_page = 2
+
+
+class NewsAdmin(admin.ModelAdmin):
+	"""News的列展示"""
+	list_display = ('id', 'title', 'thumbnail', 'pub_time', 'author',)
+	list_filter = ('title', 'author')
+	search_fields = ('title', 'author')
+	list_per_page = 2
+
+
+class BannerAdmin(admin.ModelAdmin):
+	"""NBanner的列展示"""
+	list_display = ('id', 'image_url', 'priority', 'link_to', 'pub_time')
+	list_filter = ('priority',)
+	search_fields = ('priority',)
+	list_per_page = 2
+
+
+class CommentAdmin(admin.ModelAdmin):
+	"""Comment的列展示"""
+	list_display = ('id', 'content', 'pub_time', 'news', 'author',)
+	list_filter = ('news', 'author',)
+	search_fields = ('content', 'news', 'author',)
+	list_per_page = 2
+
+admin.site.register(models.NewCategory, NewCategoryAdmin)
+admin.site.register(models.News, NewsAdmin)
+admin.site.register(models.Banner, BannerAdmin)
+admin.site.register(models.Comment, CommentAdmin)
--- apps/xfzauth/admin.py
+++ apps/xfzauth/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from . import models
 
-# admin.site.register(models.User)
-# admin.site.register(models.UserManager)
+
+class UserAdmin(admin.ModelAdmin):
+	"""user的列展示"""
+	list_display = ('id', 'telephone', 'username', 'email', 'is_active', 'gender', 'date_joined', 'is_staff')
+	list_filter = ('gender', 'is_staff', 'is_active')
+	search_fields = ('telephone', 'username', 'email')
+	list_per_page = 2
+	list_select_related = True
+
+admin.site.register(models.User, UserAdmin)
