23e9e35ea4fea81f3d880bfa35752f94531470ae
achjiang <htxz_jiang@163.com>
2018-11-11 23:53:52 +0800

    add cms:test

--- apps/cms/tests.py
+++ apps/cms/tests.py
@@ -1,10 +1,11 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, RequestFactory
 
 
 class NewsTest(TestCase):
 	"""新闻列表测试"""
 	def setUp(self):
 		self.client = Client()
+		self.factory = RequestFactory()
 
 	def test_news(self):
 		pass
