c67285966ab0811d92e56ef40940f8b0048f2c8e
achjiang <htxz_jiang@163.com>
2018-11-18 23:57:53 +0800

    pep8

--- apps/xfzauth/admin.py
+++ apps/xfzauth/admin.py
@@ -3,11 +3,20 @@ from . import models
 
 
 class UserAdmin(admin.ModelAdmin):
-	"""user的列展示"""
-	list_display = ('id', 'telephone', 'username', 'email', 'is_active', 'gender', 'date_joined', 'is_staff')
-	list_filter = ('gender', 'is_staff', 'is_active')
-	search_fields = ('telephone', 'username', 'email')
-	list_per_page = 2
-	list_select_related = True
+    """user的列展示"""
+    list_display = (
+        'id',
+        'telephone',
+        'username',
+        'email',
+        'is_active',
+        'gender',
+        'date_joined',
+        'is_staff')
+    list_filter = ('gender', 'is_staff', 'is_active')
+    search_fields = ('telephone', 'username', 'email')
+    list_per_page = 2
+    list_select_related = True
+
 
 admin.site.register(models.User, UserAdmin)
