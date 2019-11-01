46f88d8a85a0d63c0ae0c5b6ba8f99dd758f6234
achjiang <htxz_jiang@163.com>
2019-11-01 17:52:52 +0800

    del annotation

--- apps/xfzauth/decorators.py
+++ apps/xfzauth/decorators.py
@@ -22,41 +22,6 @@ def xfz_login_required(func):
 # 返回一个文件名，这样就可以写成一个装饰器
 
 
-"""
-def xfz_permission_required(model):
-
-	def decorator(viewfunc):
-		@wraps(viewfunc)
-		def _wrapper(request,*args,**kwargs):
-			content_type = ContentType.objects.get_for_model(model)
-			permissions = Permission.objects.filter(content_type=content_type)
-
-			codenames = [content_type.app_label+"."+permissions.codename for permission in permissions]
-
-			# has_perms：只能采用字符串的形式判断
-			# 字符串的形式为：app_label.codename
-			result = request.user.has_perms(codenames)
-			if result:
-				return viewfunc(request,*args,**kwargs)
-			else:
-				raise Http404()
-		return _wrapper
-	return decorator
-
-def xfz_superuser_required(viewfunc):
-
-	@wraps(viewfunc)
-	def _wrapper(request,*args,**kwargs):
-		if request.user.is_superuser:
-			return viewfunc(request,*args,**kwargs)
-		else:
-			raise Http404()
-	return _wrapper
-"""
-
-"""t代码"""
-
-
 def xfz_permission_required(model):
     def decorator(viewfunc):
         @wraps(viewfunc)
