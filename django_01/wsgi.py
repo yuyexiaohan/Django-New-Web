b39ab992e00389718cd48124f9cfb155440a84ba
achjiang <htxz_jiang@163.com>
2019-11-14 10:15:10 +0800

    del annotation

--- django_01/wsgi.py
+++ django_01/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 
 from django.core.wsgi import get_wsgi_application
+import os
 
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_01.settings")
 
 application = get_wsgi_application()
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_01.settings")
