c7e95690408cb7ac53e5534dd228c3e75472c745
achjiang <htxz_jiang@163.com>
2019-12-06 11:06:49 +0800

    change notation

--- apps/cms/forms.py
+++ apps/cms/forms.py
@@ -26,15 +26,9 @@ class WriteNewsForm(forms.ModelForm, FormMixin):
         }
 
 
-'''定义一个编辑新闻的表单函数'''
-# 可以继承WriteNewsForm
-
-
 class EditNewsForm(WriteNewsForm):
+    """编辑新闻"""
     pk = forms.IntegerField()
-
-
-'''定义一个轮播图的数据表单，用来存放前端post获取的数据，然后便于存放在数据库对应的模型中'''
 
 
 class AddBannerForm(forms.ModelForm, FormMixin):
@@ -45,10 +39,8 @@ class AddBannerForm(forms.ModelForm, FormMixin):
         fields = ('image_url', 'link_to', 'priority')
 
 
-'''定义一个编辑轮播图的表单，用来pk作为轮播图id，传入轮播图链接，跳转链接，优先级等信息'''
-
-
 class EditBannerForm(forms.ModelForm, FormMixin):
+    """用来pk作为轮播图id，传入轮播图链接，跳转链接，优先级等信息"""
     pk = forms.IntegerField()  # 因为模板中定义Id，这里表单中需要自己定义
 
     class Meta:
@@ -56,10 +48,10 @@ class EditBannerForm(forms.ModelForm, FormMixin):
         fields = ('image_url', 'link_to', 'priority')
 
 
-'''定义一个发布课程表单，内容有课程标题/课程分类/讲师/视频地址/封面图/价格/课程时长/课程简介等'''
-
-
 class EditCoursesCategoryForm(forms.Form, FormMixin):
+    """
+    发布课程表单，内容有课程标题/课程分类/讲师/视频地址/封面图/价格/课程时长/课程简介等
+    """
     pk = forms.IntegerField(error_messages={"required": "必须传入一个分类id!"})
     name = forms.CharField(max_length=100, min_length=1)
 
@@ -71,7 +63,8 @@ class AddCourseForm(forms.ModelForm, FormMixin):
 
     class Meta:
         model = Course
-        # 我们需要表单获取模型中的参数，这里使用exclude方法，排除一些参数，保留其它未排除的参数
+        # 表单获取模型中的参数，
+        # 这里使用exclude方法，排除一些参数，保留其它未排除的参数
         exclude = ('pub_time', 'category', 'teacher')
 
 
