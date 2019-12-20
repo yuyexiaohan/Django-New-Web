903b4fc00800c1eda5852e13c3eca2034bcdd892
achjiang <htxz_jiang@163.com>
2019-12-20 22:50:03 +0800

    替换django-rest-framework HTML界面

--- static/js/course_teacher.js
+++ static/js/course_teacher.js
@@ -1,10 +1,10 @@
-
+// 获取cookies
 function getCookies(name) {
     var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
-    return r ?r[1] :undefined;
+    return r ? r[1] : undefined;
 }
 
-// 老师信息获取
+// 获取老师信息
 $(document).ready(function () {
     $.get("/cms/course_teacher_list/?format=json", function (resp) {
         if(resp){
@@ -15,11 +15,43 @@ $(document).ready(function () {
     });
 });
 
-// 创建老师信息
-$(document).ready(function () {
+// 添加老师信息
+$(function () {
+   var  submitBtn = $("#submit-btn-add-teacher");
+    submitBtn.click(function (event) {
+        event.preventDefault();
+        var teacherName = $("input[name='teacher-name']").val();
+        var teacherJobTitle = $("input[name='teacher-job-title']").val();
+        var teacherProfile = $("textarea[name='teacher-profile']").val();
+        var teacherAvatar = $("input[name='teacher-avatar']").val();
+        var data = {
+               'username': teacherName,
+               'jobtitle': teacherJobTitle,
+                'profile': teacherProfile,
+                'avatar': teacherAvatar
+            };
+        $.ajax({
+            url: '/cms/course_teacher_list/',
+            type: 'post',
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            dataType: "json",
+            headers: {
+               "X-CSRFToken": getCookies("csrf_token")
+            },
+            success: function (resp) {
+                // console.log("resp:", resp);
+                if(resp){
+                    console.log("数据提交成功！");
+                    window.location.reload(); // 重新加载当前界面
+                }else{
+                    console.log("添加老师信息失败！");
+                }
+            }
+        });
+    });
+});
 
-
-   $.post("/cms/course_teacher_list/?format=json", function (resp) {
-       
-   }) ;
+$(function () {
+    // var editBtn =
 });
