8d2375496b2ac1a6ca3441136fd4603ba2a9af59
achjiang <htxz_jiang@163.com>
2020-05-13 11:21:32 +0800

    添加付费资讯js

--- static/js/write_pay_info.js
+++ static/js/write_pay_info.js
@@ -1,32 +1,9 @@
-/*
-// 缩略图文件验证及文件上传到我们自己的服务器
-$(function () {
-    var uploadBtn = $('#upload-btn');
-    // 这里不同之前需要监听click点击事件，这里只需要监听打开文件选择上传的动作，这里使用change ,event是指当前点击的事件
-    uploadBtn.change(function (event) {
-        // event.preventDefault(); // 阻止将表单发送到数据库的默认行为。但是这里使用了input的file数据类型，该标签不会进行默认操作，所以这里也可以省去。
-        var file = this.files[0]; //使用this直接指向这个文件，获取该文件
-        var formData = new FormData(); //构建成表单体，构造表单数据
-        formData.append('upfile',file); // 获取表单数据，获取变量取决于后端定义的变量'upfile'
-        // 使用Ajax将数据传输进去
-        xfzajax.post({
-            'url':'/cms/upload_file/',
-            'data':formData,
-            'processData':false, //'processDate'是一个默认为Ture的用来处理data中的数据，这里配置围为false，不让做相关处理
-            'contentType':false, // 默认也是Ture,会将文件变成一种js文件，这里实际是上传一张照片，所以设置为false不做处理
-            'success': function (result) {
-                if (result['code'] === 200){
-                    // 默认在网页中答应查看result
-                    // console.log(result);
-                    var url = result['data']['url'];
-                    var thumbnailInput = $("input[name='thumbnail']");
-                    thumbnailInput.val(url);
-                }
-            }
-        });
-    });
-});
-*/
+// 获取cookies
+function getCookies(name) {
+    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+    return r ? r[1] : undefined;
+
+}
 
 
 // 使用七牛云上传文件
@@ -53,8 +30,10 @@ $(function () {
         var domain = 'http://achjiang.cn/';
         // 七牛云给出存储空间的外链，可以根据使用七牛云空间的不同进行更改
         var url = domain + key;
-        var thumbnailInput = $("input[name='thumbnail']");
-        thumbnailInput.val(url);
+        var thumbnailInput = $("textarea[name='thumbnail']");
+        thumbnailInput.html(url);
+        console.log("上传七牛云图片地址：",url);
+        $('#teacher-button').click();
 
         progressGroup.hide(); // 进度条完成后隐藏
         progressBar.css({"width":'0'}); // 上传完成后，就将宽度调整为0
@@ -95,56 +74,66 @@ $(function () {
     });
 });
 
-// 文本编辑器
-$(function () {
-    // window.ue 是将ue这个变量作为全局变量来定义
-    window.ue = UE.getEditor('editor',{
-        // 官方文档中给出前端的高度配置说明
-        "initialFrameHeight":400 , //初始化编辑器宽度
-        'serverUrl':'/ueditor/upload/' // 服务器统一请求接口路径
-    });
-});
 
-// 新闻发布
-$(function () {
-    var submiBtn = $('#submit-btn');
-    submiBtn.click(function (event) {
-        event.preventDefault(); // 去除原有的格式
-        var btn = $(this); // 当前点击的这个按钮
-        var title = $("input[name='title']").val();
-        var desc = $("input[name='desc']").val();
-        var category = $("select[name='category']").val();
-        var thumbnail = $("input[name='thumbnail']").val();
-        var content = window.ue.getContent();
-        var news_id = btn.attr('data-news-id');
-        var url = '';
-        if(news_id){
-            url = '/cms/edit_news/';
-        }else {
-            url = '/cms/write_news/'
-        }
-        xfzajax.post({
-            'url':url,
-            'data': {
-                'title':title,
-                'desc':desc,
-                'category':category,
-                'thumbnail':thumbnail,
-                'content':content,
-                'pk':news_id
-            },
 
-            'success':function (result) {
-                if (result['code'] === 200){
+// 编辑付费知识信息
+function submitBtn(event, payInfoId) {
+    event.preventDefault();
+    var method;
+    var payInfoTitle = $("#pay-info-title").val();
+    var payInfoProfile = $("#pay-info-profile").val();
+    var payInfoPrice = $("#pay-info-price").val();
+    var payInfoPath = $("#thumbnail-form").val();
+    data = {
+        'title': payInfoTitle,
+        'profile': payInfoProfile,
+        'price': payInfoPrice,
+        'path': payInfoPath
+    };
 
-                    xfzalert.alertSuccess('新闻发布成功！',function () {
-                        url = '/cms/news_list/';
-                        window.location.reload(); // 重新加载当前界面
-
-                    });
-                }
+    if(payInfoId !== ''){
+        url = 'cms/pay_info/' + payInfoId + '/'
+    }else{
+        url = 'cms/payinfo/'
+    }
+    if (window.flag === 0){
+        method = 'post'
+    }
+    if (window.flag === 1){
+        method = 'put'
+    }
+    $.ajax({
+        url: url,
+        type: method,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        dataType: "json",
+        headers: {
+            "X-CSRFToken": getCookies("csrftoken")
+        },
+        success: function(resp){
+            if(resp){
+                alert("信息保存成功！")
+                window.location.reload();
+            }else{
+                alert("信息保存失败！")
             }
-        });
-    });
+        }
+    })
+
+}
+
+
+// 新增pay-info
+$(function addPayInfo() {
+    $("#submit-pay-info").click(function () {
+      submitBtn(event, '');
+    })
 });
 
+
+// 编辑pay-info
+$(function editPayInfo() {
+
+
+})
